b595e8d030b03986a7ba720acfe41b0759dfe427
hyunjin Lee <web.creastory@gmail.com>
2024-03-23 02:05:46 +0900

    Design: README 및 Dark모드 글씨색 변경

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils"
 
 // cva 함수를 사용하여 버튼의 다양한 변형과 크기를 정의합니다.
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
