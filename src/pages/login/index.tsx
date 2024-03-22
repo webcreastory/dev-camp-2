11b7577e38bfda139ae8a78101595ec40c39bb58
hyunjin Lee <web.creastory@gmail.com>
2024-03-22 17:17:43 +0900

    Style: Main Logo 수정

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -86,6 +86,24 @@ export default function Login() {
                 </p>
             </div>
 
+            <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+                <a
+                    className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+                    href="https://webcreastory.tistory.com/302"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        src="/devcamp-logo.png"
+                        alt="DevCamp Logo"
+                        className="dark:invert"
+                        width={400}
+                        height={24}
+                        priority
+                    />
+                </a>
+            </div>
+
             <Card className={cn('w-[600px]')}>
                 <CardHeader>
                     <CardTitle>LOGIN</CardTitle>
