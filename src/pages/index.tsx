847cf76746124a9e82186a43f464fd2adb406289
hyunjin Lee <web.creastory@gmail.com>
2024-03-22 17:02:19 +0900

    Feat: hover, line-none 효과

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home() {
                     </a>
                 </div>
                 <p
-                    className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-200 lg:p-4 lg:dark:bg-zinc-800/30 cursor-pointer hover:underline"
+                    className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-200 lg:p-4 lg:dark:bg-zinc-800/30 cursor-pointer"
                     onClick={handlePreviousProject}
                 >
                     <code className="font-mono font-bold">PREVIOUS PROJECT_GIFT FUNDING SERVICE</code>
